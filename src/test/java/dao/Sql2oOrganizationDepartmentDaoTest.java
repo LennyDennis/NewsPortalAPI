8bcf4ba7573b8584f614b9e70834dc824c8343b1
LennyDennis <lennydennis@gmail.com>
2020-02-19 08:39:54 +0300

    Add the user,department and user routes

---
+++ src/test/java/dao/Sql2oOrganizationDepartmentDaoTest.java
@@ -0,0 +1,32 @@
+package dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Sql2oOrganizationDepartmentDaoTest {
+
+    @Test
+    public void addDepartment() {
+    }
+
+    @Test
+    public void getAllDepartments() {
+    }
+
+    @Test
+    public void findDepartmentById() {
+    }
+
+    @Test
+    public void updateDepartment() {
+    }
+
+    @Test
+    public void deleteDepartmentById() {
+    }
+
+    @Test
+    public void clearAllDepartments() {
+    }
+}
